73b0d3cf59b439768aaedf91612319c14228b995
WatWittawat <wittawat.pho@ku.th>
2024-12-02 02:22:42 +0700

    feat: add registration dialog and update font

---
+++ src/components/UI/MyDialog.tsx
@@ -0,0 +1,25 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../UI/dialog";
+
+type Props = {
+  isOpen: boolean;
+  title: string;
+  children: React.ReactNode;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MyDialog = ({ isOpen, title, setIsOpen, children }: Props) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="self-center text-2xl font-medium">
+            {title}
+          </DialogTitle>
+          {children}
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MyDialog;
