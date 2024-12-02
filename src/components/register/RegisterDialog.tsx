9381d54016bf1ea34f818964f2c493dc79711905
WatWittawat <wittawat.pho@ku.th>
2024-12-02 17:37:41 +0700

    fix: correct import path for Button component in RegisterDialog

--- src/components/register/RegisterDialog.tsx
+++ src/components/register/RegisterDialog.tsx
@@ -3,7 +3,6 @@ import MyDialog from "../ui/MyDialog";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,6 +16,7 @@ import { Input } from "@/components/ui/input";
 import { useEffect } from "react";
 import { MailIcon } from "lucide-react";
 import { PasswordInput } from "../ui/password_input";
+import { Button } from "../ui/button";
 
 type Props = {
   isOpen: boolean;
