dc8a3ee729455893dd942b770a2dbf8545c0b4fa
WatWittawat <wittawat.pho@ku.th>
2024-12-02 17:26:07 +0700

    feat: implement form components and integrate with registration dialog

--- src/components/register/RegisterDialog.tsx
+++ src/components/register/RegisterDialog.tsx
@@ -1,85 +1,152 @@
 "use client";
-import { useState } from "react";
-import MyDialog from "../UI/MyDialog";
+import MyDialog from "../ui/MyDialog";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useEffect } from "react";
+import { MailIcon } from "lucide-react";
+import { PasswordInput } from "../ui/password_input";
 
 type Props = {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const RegisterDialog = ({ isOpen, setIsOpen }: Props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6, {
+      message: "",
+    }),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: "Password and confirm password must match",
+      path: ["confirmPassword"],
+    }
+  );
 
-  const handleRegister = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Registering:", { email, password });
-  };
+const RegisterDialog = ({ isOpen, setIsOpen }: Props) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      form.reset();
+    }
+  }, [isOpen, form]);
 
   return (
     <MyDialog isOpen={isOpen} title="สมัครสมาชิก" setIsOpen={setIsOpen}>
-      <form onSubmit={handleRegister} className="space-y-4 w-full">
-        <div className="my-5">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm placeholder:font-light"
-            required
-          />
-        </div>
-        <div className="my-5">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm placeholder:font-light"
-            required
-          />
-        </div>
-        <div className="my-5">
-          <label
-            htmlFor="confirmPassword"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Confirm Password
-          </label>
-          <input
-            type="confirmPassword"
-            id="confirmPassword"
-            value={confirmPassword}
-            placeholder="Confirm your password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm placeholder:font-light"
-            required
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 w-full"
+        >
+          <div className="my-5">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter email"
+                      className="focus-visible:ring-0 focus:border-orange-400"
+                      {...field}
+                      suffix={<MailIcon color="gray" />}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="my-5">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      className="focus-visible:ring-0 focus:border-orange-400"
+                      {...field}
+                      placeholder="Enter password"
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Password must be at least 6 characters
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="my-5">
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      className="focus-visible:ring-0 focus:border-orange-400"
+                      {...field}
+                      placeholder="Confirm password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <p className="flex justify-center items-center text-sm text-gray-400">
+            Already have an account?
+            <span>
+              <Button
+                type="button"
+                variant="link"
+                onClick={() => console.log("Hello")}
+              >
+                Login
+              </Button>
+            </span>
+          </p>
+          <Button type="submit" className="w-full">
             Register
-          </button>
-        </div>
-      </form>
+          </Button>
+        </form>
+      </Form>
     </MyDialog>
   );
 };
