0dbd030bfb815cfb712a7c81e13859f761603dc1
ponzaa567 <ponpon13173@gmail.com>
2024-12-06 01:58:19 +0700

    connect register and login toast loggic

---
+++ src/app/api/auth/Myauth/Register/route.ts
@@ -0,0 +1,21 @@
+import { FindUserByEmail } from "@/lib/backend/myAuth/Command/Login";
+import { CreateUser } from "@/lib/backend/myAuth/Command/Register";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  console.log("====================   Register   =============");
+  var body = await req.json();
+  const { email, password, confirmPassword } = body;
+
+  var user = await FindUserByEmail(email);
+  // email already exists
+  console.log( "user : ", user);  
+  if (user)
+    return NextResponse.json(
+      { message: "Email already exists" },
+      { status: 401 }
+    );
+  // create user
+  var newUser = await CreateUser(email, password);
+  return NextResponse.json(newUser , { status: 201 });
+}
--- src/lib/backend/myAuth/Command/Register.ts
+++ src/lib/backend/myAuth/Command/Register.ts
@@ -1,4 +1,6 @@
+import { User } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import { prisma } from "../../prisma";
 
 export async function encryptPassword(password: string): Promise<string> {
   try {
@@ -10,3 +12,25 @@ export async function encryptPassword(password: string): Promise<string> {
     throw error; // Optionally rethrow the error
   }
 }
+
+export async function CreateUser(email: string, password: string): Promise<User> 
+{
+  try {
+    const hashedPassword = await encryptPassword(password);
+    const newUser = await prisma.user.create<User>({
+      data: {
+        email: email,
+        password: hashedPassword,
+        emailVerified: null,
+        image: null,
+        name: null,
+        updatedAt: new Date(),
+      },
+    });
+    return newUser;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+}
+
