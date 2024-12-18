751abcaa0dc8c8696e9289f05bc3c74025f0ff4d
WatWittawat <wittawat.pho@ku.th>
2024-12-19 01:28:50 +0700

    Add trip planning feature to profile page: implement trip navigation and create trip button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { BoomBox, Images, Pencil, UserPen } from "lucide-react";
+import { BoomBox, CirclePlus, Images, Pencil, UserPen } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -9,6 +9,7 @@ import { useState } from "react";
 const Profile = () => {
   const [media, setMedia] = useState("photos");
   const [draft, setDraft] = useState("draft");
+  const [trip, setTrip] = useState("all");
   const router = useRouter();
   return (
     <div className="bg-slate-200 w-full h-full">
@@ -122,7 +123,55 @@ const Profile = () => {
         </div>
       </div>
 
-      <div></div>
+      <div className="flex flex-col w-full bg-white gap-5 p-3">
+        <div className="flex flex-row justify-between">
+          <Image src="/logo.png" alt="logo" width={100} height={100} />
+          <Button>
+            <CirclePlus />
+            Create trip plan
+          </Button>
+        </div>
+        <nav>
+          <ul className="flex justify-center gap-3 flex-wrap">
+            <li>
+              <Button
+                variant="link"
+                className={`transition duration-300 ease-in-out  ${trip === "all" ? "bg-orange-400 text-white" : "text-black"} `}
+                onClick={() => setTrip("all")}
+              >
+                <p className="text-xs">
+                  All trips <br />({0})
+                </p>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                className={`transition duration-300 ease-in-out  ${trip === "withme" ? "bg-orange-400 text-white" : "text-black"} `}
+                onClick={() => setTrip("withme")}
+              >
+                <p className="text-xs">
+                  Shared with me <br />({1})
+                </p>
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="link"
+                className={`transition duration-300 ease-in-out  ${trip === "upcoming" ? "bg-orange-400 text-white" : "text-black"} `}
+                onClick={() => setTrip("upcoming")}
+              >
+                <p className="text-xs">
+                  Upcoming trip <br />({2})
+                </p>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex h-80 justify-center items-center w-full ">
+          There is no media to show.
+        </div>
+      </div>
     </div>
   );
 };
