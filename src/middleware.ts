9785ea65e1324a81f91f3053f3f82d9e20b786f6
ponzaa567 <ponpon13173@gmail.com>
2024-12-22 22:18:58 +0700

    [Deploy] Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,17 @@
-// import { getToken } from "next-auth/jwt";
-// import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function middleware(req: NextRequest) {
-//   console.log("================ MiddleWare ==================");
-//   const user = await getToken({
-//     req: req,
-//     secret: process.env.NEXTAUTH_SECRET,
-//   });
-//   if (user == null) {
-//     return NextResponse.redirect(
-//       new URL("http://localhost:3000/api/auth/signin")
-//     );
-//   }
-// }
-// export const config = {
-//   matcher: "/profile/:path*", // Define which routes should use the middleware
-// };
+export async function middleware(req: NextRequest) {
+  const baseUrl = req.nextUrl.origin;
+  console.log("================ MiddleWare ==================");
+  const user = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  if (user == null) {
+    return NextResponse.redirect(new URL(`${baseUrl}/api/auth/signin`));
+  }
+}
+export const config = {
+  matcher: "/profile/:path*", // Define which routes should use the middleware
+};
