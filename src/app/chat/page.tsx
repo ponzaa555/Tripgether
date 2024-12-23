7b56b4969138cf92307d98f4458c79f1a69fb4a3
ponzaa567 <ponpon13173@gmail.com>
2024-12-24 00:27:26 +0700

    [Test] Fix Chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
 import ChatLayout from "@/src/components/chat_page/chat_layout";
 import ConvarsationFallback from "@/src/components/chat_page/conversation/ConvarsationFallback";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 const ChatPage = () => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log("waiting ChatPage", session)
+  },[session])
+
+  if(session?.user.id == null){
+    return <p>Loading.....</p>
+  }
   return (
     <ChatLayout>
       <ConvarsationFallback />
