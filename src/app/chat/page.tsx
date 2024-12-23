2bbe04710f3db72a12203e0c5111e50d31d48cfb
ponzaa567 <ponpon13173@gmail.com>
2024-12-24 01:07:39 +0700

    [Fix] Chat session not have id

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,18 +3,21 @@
 import ChatLayout from "@/src/components/chat_page/chat_layout";
 import ConvarsationFallback from "@/src/components/chat_page/conversation/ConvarsationFallback";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const ChatPage = () => {
   const { data: session } = useSession();
+  let count = 0
 
   useEffect(() => {
     console.log("waiting ChatPage", session)
+    if(session?.user.id == null){
+      new Promise((resolve) => setTimeout(resolve , 3000)) 
+      window.location.reload();
+    }
   },[session])
 
-  if(session?.user.id == null){
-    return <p>Loading.....</p>
-  }
   return (
     <ChatLayout>
       <ConvarsationFallback />
