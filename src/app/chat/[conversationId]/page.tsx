87c23a73c3108bb1554982e149e967ae122e1b53
ponzaa567 <ponpon13173@gmail.com>
2024-12-28 16:45:11 +0700

    [FIx]app chat change get session to getServerSession

--- src/app/chat/[conversationId]/page.tsx
+++ src/app/chat/[conversationId]/page.tsx
@@ -1,19 +1,8 @@
-"use client";
 
-import ChatLayout from "@/src/components/chat_page/chat_layout";
-import { useQuery } from "convex/react";
-import { useSession } from "next-auth/react";
-import { Loader2 } from "lucide-react";
-import ConvarsationContainer from "@/src/components/chat_page/conversation/ConvarsationContainer";
 import { Id } from "@/convex/_generated/dataModel";
-import { api } from "@/convex/_generated/api";
-import Header from "@/src/components/chat_page/conversation/Header";
-import Body from "@/src/components/chat_page/conversation/Body";
-import ChatInput from "@/src/components/chat_page/conversation/ChatInput";
-import { useState } from "react";
-import RemoveFriendDialog from "@/src/components/chat_page/dialogs/RemoveFriendDialog";
-import DeleteGroupDialog from "@/src/components/chat_page/dialogs/DeleteGroupDialog";
-import LeaveGroupDialog from "@/src/components/chat_page/dialogs/LeaveGroupDialog";
+import { getServerSession } from "next-auth";
+import ChatConversationComponent from "./_component/ChatConversationComponent";
+import authOption from "@/src/lib/backend/authOption";
 
 type Props = {
   params: {
@@ -21,94 +10,14 @@ type Props = {
   };
 };
 
-const ChatConversationPage = ({ params: { conversationId } }: Props) => {
-  const { data: session } = useSession();
-  const conversation = useQuery(api.conversation.get, {
-    id: conversationId,
-    currentUserId: session?.user?.id!,
-  });
-  const [removeFriendDialogOpen, setRemoveFriendDialogOpen] = useState(false);
-  const [deleteGroupDialogOpen, setDeleteGroupDialogOpen] = useState(false);
-  const [leaveGroupDialogOpen, setLeaveGroupFriendDialogOpen] = useState(false);
-  const [callType, setCallType] = useState<"audio" | "video" | null>(null);
-
-  return (
-    <ChatLayout userID={session?.user.id!}>
-      {conversation === undefined ? (
-        <div className="w-full h-full flex items-center justify-center">
-          <Loader2 className="animate-spin" />
-        </div>
-      ) : conversation === null ? (
-        <p className="w-full h-full flex items-center justify-center">
-          Conversation not found
-        </p>
-      ) : (
-        <ConvarsationContainer>
-          <RemoveFriendDialog
-            conversationId={conversationId}
-            open={removeFriendDialogOpen}
-            setOpen={setRemoveFriendDialogOpen}
-          />
-          <DeleteGroupDialog
-            conversationId={conversationId}
-            open={deleteGroupDialogOpen}
-            setOpen={setDeleteGroupDialogOpen}
-          />
-          <LeaveGroupDialog
-            conversationId={conversationId}
-            open={leaveGroupDialogOpen}
-            setOpen={setLeaveGroupFriendDialogOpen}
-          />
-          <Header
-            name={
-              (conversation.isGroup
-                ? conversation.name
-                : conversation.otherMember?.username) || ""
-            }
-            imageUrl={
-              conversation.isGroup
-                ? undefined
-                : conversation.otherMember?.imageUrl
-            }
-            options={
-              conversation.isGroup
-                ? [
-                    {
-                      label: "Leave group",
-                      destructive: false,
-                      onClick: () => setLeaveGroupFriendDialogOpen(true),
-                    },
-                    {
-                      label: "Delete group",
-                      destructive: true,
-                      onClick: () => setDeleteGroupDialogOpen(true),
-                    },
-                  ]
-                : [
-                    {
-                      label: "Remove friend",
-                      destructive: true,
-                      onClick: () => setRemoveFriendDialogOpen(true),
-                    },
-                  ]
-            }
-          />
-          <Body
-            members={
-              conversation.isGroup
-                ? conversation.otherMembers
-                  ? conversation.otherMembers
-                  : []
-                : conversation.otherMember
-                  ? [conversation.otherMember]
-                  : []
-            }
-          />
-          <ChatInput />
-        </ConvarsationContainer>
-      )}
-    </ChatLayout>
-  );
+const ChatConversationPage =  async({ params: { conversationId } }: Props) => {
+  const session = await getServerSession(authOption);
+  if(session?.user.id == null){
+    return <p>Loading ....</p>
+  }
+  return(
+    <ChatConversationComponent userId={session?.user.id as string} conversationId={conversationId as Id<"conversations">}/>
+  )
 };
 
 export default ChatConversationPage;
