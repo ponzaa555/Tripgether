848ed10f334d4c178641d04c654749a51e2100aa
WatWittawat <wittawat.pho@ku.th>
2025-01-04 18:02:26 +0700

    [Refactor] Update ChatPage component: fix userID prop naming and improve code formatting

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,20 +1,18 @@
-
-
 import ChatLayout from "@/src/components/chat_page/chat_layout";
 import ConvarsationFallback from "@/src/components/chat_page/conversation/ConvarsationFallback";
 import authOption from "@/src/lib/backend/authOption";
 import { getServerSession } from "next-auth";
 
 const ChatPage = async () => {
-  const session = await getServerSession(authOption)
+  const session = await getServerSession(authOption);
   if (session?.user.id != null) {
     return (
-      <ChatLayout userID={session.user.id}>
+      <ChatLayout userId={session.user.id}>
         <ConvarsationFallback />
       </ChatLayout>
     );
-  }else{
-    return <p>Loading...</p>
+  } else {
+    return <p>Loading...</p>;
   }
-}
+};
 export default ChatPage;
