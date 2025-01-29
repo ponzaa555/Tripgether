483a9cbd203c0465cad49eea6ab947e19889dff0
WatWittawat <wittawat.pho@ku.th>
2025-01-30 02:58:15 +0700

    [Refactor] Update blog and trip components; add search functionality and pagination; remove unused trip search hook

--- src/app/trip/page.tsx
+++ src/app/trip/page.tsx
@@ -1,33 +1,49 @@
 "use client";
 
-import BlogContent from "@/src/components/home_page/blog_content/BlogContent";
-import { Input } from "@/src/components/UI/input";
-import useTripSearch from "@/src/hooks/useTripSearch";
+import { api } from "@/convex/_generated/api";
+import BlogContent from "@/src/components/trip_page/BlogContent";
+import SearchInputComponent from "@/src/components/trip_page/SearchInputComponent";
+import { Button } from "@/src/components/UI/Button";
+import LoadingComponent from "@/src/components/UI/Loading";
+import { usePaginatedQuery } from "convex/react";
 import { useState } from "react";
-
 const TripPage = () => {
   const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
-  const { loading, error, trips, hasMore } = useTripSearch(query, pageNumber);
+  const { results, loadMore, status } = usePaginatedQuery(
+    api.blog.infiniteScrollAndSearch,
+    { query: query },
+    { initialNumItems: 10 }
+  );
 
-  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
-    setQuery(e.target.value);
-    setPageNumber(1);
+  function handleSearch(query: string) {
+    setQuery(query);
+  }
+
+  if (status === "LoadingFirstPage") {
+    return <LoadingComponent />;
   }
 
   return (
-    <div className="flex flex-col gap-5 w-full pt-24">
+    <div className="flex flex-col gap-5 w-full pt-24 pb-5">
       <h3 className="text-center text-3xl sm:text-5xl md:text-6xl font-black">
         Enjoy The Moment
       </h3>
-      <div className="px-20">
-        <Input
-          className="w-full"
-          placeholder="Search"
-          onChange={handleSearch}
-        />
+      <div className="px-20 w-full gap-2">
+        <SearchInputComponent onSearch={handleSearch} />
       </div>
-      <BlogContent />
+      <BlogContent blogs={results} />
+      <div className="p-10 flex justify-center">
+        {status !== "Exhausted" ? (
+          <Button
+            onClick={() => loadMore(10)}
+            disabled={status !== "CanLoadMore"}
+          >
+            {status === "CanLoadMore" ? "Load More" : "Loading..."}
+          </Button>
+        ) : (
+          <p className="text-3xl">----- No data available -----</p>
+        )}
+      </div>
     </div>
   );
 };
